a9b14ada00ff5b8ca5e5da64a8460f20ef13fbee
TanDongXu <sa614149@mail.ustc.edu.cn>
2016-12-24 15:18:00 +0800

    add some function

--- layers/layer.cpp
+++ layers/layer.cpp
@@ -13,12 +13,76 @@
 using namespace std;
 
 template<typename Ntype>
-inline Ntype Layer<Ntype>::Forward()
+Layer<Ntype>* LayerContainer<Ntype>::getLayerByName(string name)
 {
+    if(m_layersMap.find(name) != m_layersMap.end())
+    {
+        return m_layersMap[name];
+    }else
+    {
+        LOG(FATAL) << "Layer " << name << " is not in the layersContainer.";
+        return NULL;
+    }
 }
 
+//  Story the layers into map
 template<typename Ntype>
-inline void Layer<Ntype>::Backward()
+void LayerContainer<Ntype>::storelayer(string prevlayerName, string name, Layer<Ntype>* layer)
 {
+    if(m_layersMap.find(name) == m_layersMap.end())
+    {
+        m_layersMap[name] = layer;
+        storeName(name);
+
+        // Create a linked list
+        if(string("NULL") == prevlayerName)
+        {
+            m_layersMap[name]->getPrevLayerConfig().clear();
+            m_layersMap[name]->setInputName(" ");
+        }else
+        {
+            m_layersMap[name]->setInputName(prevlayerName);
+            //cout<<"prevName: "<<prev_name<<" name: "<<name<<endl;
+            m_layersMap[prevlayerName]->insertNextLayer(m_layersMap[name] );
+            m_layersMap[name]->insertPrevLayer(m_layersMap[prevlayerName]);
+        }
+
+    }else
+    {
+        LOG(FATAL) << name <<" has already in layersMap.";
+    }
 }
 
+/*
+ * overload
+ * Linear storage layer
+ */
+template<typename Ntype>
+void LayerContainer<Ntype>::storelayer(string name, Layer<Ntype>* layer)
+{
+    if(m_layersMap.find(name) == m_layersMap.end())
+    {
+        m_layersMap[name] = layer;
+        storeName(name);
+
+        // Create a linked list
+        if(1 == m_layersMap.size())
+        {
+            m_layersMap[name]->getPrevLayerConfig().clear();
+            m_layersMap[name]->setInputName(" ");
+
+        }else
+        {
+            m_layersMap[name]->setInputName(m_layersMap[m_layersName[m_layersName.size() - 2]]->getName());
+            m_layersMap[m_layersName[m_layersName.size() -2 ]]->insertNextLayer(m_layersMap[name]);
+            m_layersMap[name]->insertPrevLayer(m_layersMap[m_layersName[m_layersName.size() - 2]]);
+        }
+    }else
+    {
+        LOG(FATAL) << name <<" has already in layersMap.";
+    }
+}
+
+
+INSTANTIATE_CLASS(Layer);
+INSTANTIATE_CLASS(LayerContainer);
--- layers/layer.hpp
+++ layers/layer.hpp
@@ -10,7 +10,10 @@
 
 #include<iostream>
 #include<vector>
+#include<map>
 #include<string>
+#include<glog/logging.h>
+#include"common/common.hpp"
 #include"common/nDMatrix.hpp"
 #include"config/configBase.hpp"
 
@@ -24,20 +27,69 @@ class Layer
     public:
     virtual ~Layer(){}
     // Adjust the shapes of top NDMatrix and internal buffers to accommodate the shapes of the bottom
-    virtual void ReShape() = 0;
     // Given the bottom NDMatrix, compute the top NDMatrix and the loss
-    virtual Ntype Forward() = 0;
+    virtual Ntype Forward(Phase phase) = 0;
     // given the top NDMatrix error gradients, compute the bottom NDMatrix error gradients
     virtual void Backward() = 0;
-    // Rerturn the layer type
-    virtual inline const string type() const{ return""; }
-    
-    protected:
+    inline void insertPrevLayer(Layer<Ntype>* layer){ CHECK(layer); m_prevLayer.push_back(layer); }
+    inline void insertNextLayer(Layer<Ntype>* layer){ CHECK(layer); m_nextLayer.push_back(layer); }
+    inline vector<Layer<Ntype>*>& getPrevLayerConfig() { return m_prevLayer; }
+    inline vector<Layer<Ntype>*>& getNextLayerConfig() { return m_nextLayer; }
+    inline void setInputName(string name){ m_inputName = name; }
+    inline string getName() const { return m_name; }
+    inline NDMatrix<Ntype>* getTop() const { return m_top; }
+
+    protected: 
+    virtual void ReShape() = 0;
+    string m_name;
+    string m_inputName;
+    Ntype m_loss;
+    float m_lrate;
+    int m_inputChannels;
+    int m_number;
+    int m_channels;
+    int m_height;
+    int m_width;
     NDMatrix<Ntype>* m_bottom;
     NDMatrix<Ntype>* m_top;
-    Ntype m_loss;
-    vector<BaseLayerConfig*> m_prevLayer;
-    vector<BaseLayerConfig*> m_nextLayer;
+    vector<Layer<Ntype>*> m_prevLayer;
+    vector<Layer<Ntype>*> m_nextLayer;
 };
 
+template<typename Ntype>
+class LayerContainer
+{
+    public:
+    static LayerContainer<Ntype>* getInstance()
+    {
+        static LayerContainer<Ntype>* container = new LayerContainer<Ntype>();
+        return container;
+    }
+
+    Layer<Ntype>* getLayerByName(string name);
+    void storelayer(string name, Layer<Ntype>* layer);
+    void storelayer(string prevlayerName, string name, Layer<Ntype>* layer);
+    inline void storeName(string name) { m_layersName.push_back(name);}
+    inline string getNameByIndex(int index)
+    {
+        CHECK_LT(index, m_layersName.size()) << "The index out of layers size.";
+        return m_layersName[index];
+    }
+    inline int getLayersNum() const { return m_layersMap.size(); }
+    inline bool isHasLayer(string name)
+    {
+        if(m_layersMap.find(name) != m_layersMap.end())
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+    
+    private:
+    LayerContainer(){}
+    map<string, Layer<Ntype>*> m_layersMap;
+    vector<string> m_layersName;
+};
 #endif
